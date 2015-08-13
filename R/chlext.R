1fd7fcc3d0528933b842c2f39d255fa9e35d8426
jsta <jstachelek@utexas.edu>
2015-08-13 12:21:27 -0400

    bad R files

--- R/chlext.R
+++
@@ -1,8 +0,0 @@
-#chlext.R
-survey<-commandArgs(trailingOnly=TRUE)[1]
-x<-substring(basename(survey),1,6)
-print(x)
-print(survey)
-
-suppressWarnings(library(DataflowR))
-chlmap(x)
--- R/chlextlist.R
+++
@@ -1,9 +0,0 @@
-library(DataflowR)
-
-fdir<-getOption("fdir")
-
-dt<-read.csv(file.path(fdir,"DF_GrabSamples","extractChlcoef2.csv"))
-
-sapply(dt[which(is.na(dt$notes)),"yearmon"],function(x) write.csv(x,file.path("logs",paste0(x,"fit.log")),row.names = FALSE))
-
-write.csv(dt[which(is.na(dt$notes)),"yearmon"],"yearmonfits.csv",row.names = FALSE)
