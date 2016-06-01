7a704c0dc749741b0331a91fb9a7334a031c3bf2
jsta <jstachelek@utexas.edu>
2016-06-01 14:47:07 -0400

    cleanup grab sample processing, add pvalue markers to cor matrix

--- code/allgrabs.R
+++ code/allgrabs.R
@@ -1,6 +1,6 @@
 library(DataflowR)
 fdir <- getOption("fdir")
-fathombasins<-rgdal::readOGR(file.path(fdir,"DF_Basefile/fbzonesmerge.shp"),layer="fbzonesmerge",verbose=FALSE,stringsAsFactors=FALSE)
+fathombasins <- rgdal::readOGR(file.path(fdir, "DF_Basefile/fbzonesmerge.shp"),layer = "fbzonesmerge", verbose = FALSE, stringsAsFactors = FALSE)
 goodyears <- read.csv("data/goodyears.csv", stringsAsFactors = FALSE)
 goodyears <- as.list(as.data.frame(t(goodyears)))
 
@@ -9,4 +9,41 @@ dt <- do.call("rbind", dt)
 dt <- dt[!is.na(dt$lon_dd) & !is.na(dt$lat_dd),]
 dt <- dt[dt$location %in% methods::slot(fathombasins, "data")$ZoneName,]
 
-write.csv(dt, "data/allgrabs.csv", row.names = FALSE)
+grabs <- dt
+grabs <- grabs[!(grabs$location %in% c("Deer Key", "Taylor River")),]
+
+grabs$tn <- grabs$tkn + grabs$tdkn
+grabs <- grabs[,!(names(grabs) %in% c("tkn", "tdkn"))]
+
+# fit distribution to PO4 to fill in zeros following Helsel and Hirsh ch. 13
+library(fitdistrplus)
+# STEVE SAYS THAT DETECTION LIMIT IS 0.015 uM (0.5 ppb)
+# FABIOLA SAYS THAT Solorzano METHOD DETECTION LIMIT IS 0.025 uM
+po4_detect_limit <- 0.025
+greaterthan_detect_limit <- grabs[grabs$po4 > po4_detect_limit & !is.na(grabs$po4), "po4"]
+fit.normal <- fitdist(greaterthan_detect_limit, "lnorm")
+
+get_lowerquantile <- function(quant){
+  res <- rnorm(1, fit.normal$estimate[1], fit.normal$estimate[2])
+  while(res > qnorm(quant, fit.normal$estimate[1], fit.normal$estimate[2])){
+    res <- rnorm(1, fit.normal$estimate[1], fit.normal$estimate[2])
+  }
+  res
+}
+
+lessthan_detect_limit <- grabs[grabs$po4 < po4_detect_limit & !is.na(grabs$po4), "po4"]
+grabs[grabs$po4 < po4_detect_limit & !is.na(grabs$po4), "po4"] <- exp(sapply(1:length(lessthan_detect_limit), function(x) get_lowerquantile(.1)))
+
+write.csv(grabs, "data/allgrabs.csv", row.names = FALSE)
+
+grabs$chla <- log(grabs$chla)
+
+grabs[grabs$pp < 0.05 & !is.na(grabs$pp), "pp"] <-  NA# made-up detection limit
+grabs$pp <- log(grabs$pp)
+
+
+grabs$tp <- log(grabs$tp)
+grabs$po4 <- log(grabs$po4)
+grabs$tdp <- log(grabs$tdp)
+
+write.csv(grabs, "data/allgrabs_log.csv", row.names = FALSE)
--- code/rmd_chunks/cormat.R
+++ code/rmd_chunks/cormat.R
@@ -1,26 +1,52 @@
-grabs <- read.csv(file.path("data", "allgrabs.csv"), row.names = NULL, stringsAsFactors = FALSE)
-
-
-grabs <- grabs[!(grabs$location %in% c("Deer Key", "Taylor River")),]
+grabs <- read.csv(file.path("data", "allgrabs_log.csv"), row.names = NULL, stringsAsFactors = FALSE)
 
 grabs$location <- factor(grabs$location, levels = c("Whipray Basin", "Rankin", "Terrapin Bay", "Monroe Lake", "Seven Palm Lake", "Madeira Bay", "Little Madeira", "Pond 5", "Joe Bay", "Long Sound", "Blackwater Sound", "Lake Surprise", "Manatee", "Barnes"))
 
 metadata_fields <- c("date", "time", "location", "lat_dd", "lon_dd")
 #data_fields <-   c("salt", "chla", "tss", "pp", "tp", "tdp", "po4", "toc", "doc", "tkn", "tdkn", "chlaiv", "temp", "cond", "sal", "trans", "cdom", "brighteners", "phycoe", "phycoc", "c6chl", "c6cdom", "c6turbidity", "c6temp")
 data_fields <-   c("chla", "tss", "pp", "tp", "tdp", "po4", "toc", "doc", "tkn", "tdkn")
-data_fields <-   c("pp", "tp", "tdp", "po4", "tkn", "tdkn", "chla")
+data_fields <-   c("pp", "tp", "tdp", "po4", "tn", "chla")
 
 grabs_cor <- grabs[,data_fields]
-#apply(grabs_cor, 2, function(x) sum(!is.na(x)))
 grabs_cor <- grabs_cor[complete.cases(grabs_cor),]
+
+comb_names <- expand.grid(names(grabs_cor), names(grabs_cor))
+
+p.values <- apply(comb_names, 1, function(x) round(cor.test(grabs_cor[,x[1]], grabs_cor[,x[2]])$p.value, 2))
+p.values <- matrix(p.values, ncol = ncol(grabs_cor))
+
+
 grabs_cor <- matrix(round(cor(grabs_cor), 2), ncol = ncol(grabs_cor))
+
 grabs_cor[lower.tri(grabs_cor)] <- NA
-grabs_cor[seq(from = 1, to = 49, by = 8)] <- NA
+grabs_cor[seq(from = 1, to = 36, by = 7)] <- NA
+
+p.values[lower.tri(p.values)] <- NA
+p.values[seq(from = 1, to = 36, by = 7)] <- NA
+
+#grabs_cor <- matrix(as.character(grabs_cor), ncol = ncol(grabs_cor))
+
 grabs_cor <- data.frame(grabs_cor)
 grabs_cor <- grabs_cor[1:(nrow(grabs_cor) - 1), 2:ncol(grabs_cor)]
-grabs_cor <- data.frame(matrix(
+
+p.values <- data.frame(p.values)
+p.values <- p.values[1:(nrow(p.values) - 1), 2:ncol(p.values)]
+
+grabs_cor <- data.frame(
+  matrix(
   sapply(unlist(grabs_cor), function(x) na.omit(x))
-  , ncol = ncol(grabs_cor), byrow = FALSE), row.names = names(grabs[,data_fields])[1:(length(names(grabs[,data_fields])) - 1)])
+  , ncol = ncol(grabs_cor), byrow = FALSE),
+  row.names = names(grabs[,data_fields])[1:(length(names(grabs[,data_fields])) - 1)])
+
+p.values <- data.frame(
+  matrix(
+    sapply(unlist(p.values), function(x) na.omit(x))
+    , ncol = ncol(p.values), byrow = FALSE),
+  row.names = names(grabs[,data_fields])[1:(length(names(grabs[,data_fields])) - 1)])
+
+sig.xy <- which(p.values <= 0.0001, arr.ind = TRUE)
+grabs_cor[sig.xy] <- paste0(grabs_cor[sig.xy], "*")
+
 names(grabs_cor) <- names(grabs[,data_fields])[2:length(names(grabs[,data_fields]))]
 
 knitr::kable(grabs_cor)
