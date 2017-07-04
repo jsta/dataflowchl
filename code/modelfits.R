eb5ee908dbf8ae5f35fd3f1a341c58f50dd30daa
jsta <jstachelek@utexas.edu>
2017-07-04 16:37:29 -0400

    linting

--- code/modelfits.R
+++ code/modelfits.R
@@ -1,12 +1,16 @@
 library(DataflowR)
 
-dt<-read.csv(file.path(fdir,"DF_GrabSamples","extractChlcoef2.csv"),row.names = NULL)
-dt<-dt[!is.na(dt$date),]
-dt<-dt[order(dt$yearmon),]
-dt<-dt[,c("yearmon","cdom","chlaiv","phycoe","c6chl","c6cdom","phycoc","intercept","rsquared","pvalue")]
-dt[,2:8]<-round(dt[,2:8],4)
-dt[,"rsquared"]<-round(dt[,"rsquared"],2)
-dt[,"pvalue"]<-round(dt[,"pvalue"],2)
-dt$pvalue[dt$pvalue==0]<-"< 0.00"
+dt <- read.csv(file.path(fdir,"DF_GrabSamples","extractChlcoef2.csv"), 
+               row.names = NULL)
 
-write.csv(dt,"/home/jose/Documents/Science/JournalSubmissions/DataflowChl/tables/modelfits.csv",row.names = FALSE)
+dt <- dt[!is.na(dt$date),]
+dt <- dt[order(dt$yearmon),]
+dt <- dt[,c("yearmon", "cdom", "chlaiv", "phycoe", "c6chl", "c6cdom", 
+            "phycoc", "intercept", "rsquared", "pvalue")]
+
+dt[,2:8]                  <- round(dt[,2:8],4)
+dt[,"rsquared"]           <- round(dt[,"rsquared"],2)
+dt[,"pvalue"]             <- round(dt[,"pvalue"],2)
+dt$pvalue[dt$pvalue == 0] <- "< 0.00"
+
+write.csv(dt, "tables/modelfits.csv", row.names = FALSE)
