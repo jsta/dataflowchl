3a62ec51355d373b0988a89042b0db8e47feb95b
jsta <jstachelek@utexas.edu>
2017-07-05 11:28:22 -0400

    add sample size, drop feb 2010 panel, bigger label size, closes #6

--- code/multipanel.R
+++ code/multipanel.R
@@ -1,6 +1,6 @@
 library(DataflowR)
 
-sample_dates <- c(200808, 200910, 201002, 201004, 201007, 201102, 201105, 201206, 201209, 201212, 201305, 201308, 201311, 201404, 201407, 201410, 201502, 201505, 201507, 201509)
+sample_dates <- c(200808, 200910, 201004, 201007, 201102, 201105, 201206, 201209, 201212, 201305, 201308, 201311, 201404, 201407, 201410, 201502, 201505, 201507, 201509)
 
 format_labels <- function(x){
   strftime(as.character(as.Date(paste0(x, "01"), format = "%Y%m%d")), format = "%b %Y")
@@ -8,4 +8,4 @@ format_labels <- function(x){
 
 label_strings <- sapply(as.character(sample_dates), format_labels)
   
-grassmap(rnge = sample_dates, params = 'chlext', numrow = 5, numcol = 4, labelling = TRUE, label_string = label_strings)
+grassmap(rnge = sample_dates, params = 'chlext', numrow = 5, numcol = 4, labelling = TRUE, label_string = label_strings, label_size = 35)
--- code/prep_table-2.R
+++ code/prep_table-2.R
@@ -1,17 +1,36 @@
-library(xtable)
+# library(xtable)
+library(knitr)
+library(kableExtra)
+library(magrittr)
 
 dt <- read.csv("tables/modelfits.csv", stringsAsFactors = FALSE)
 dt <- dt[order(dt$yearmon),]
 dt[dt$pvalue < 0.01 & !is.na(dt$pvalue), "pvalue"] <- "<0.01"
 dt <- dt[dt$yearmon <= 201509,]
 
+gy <- read.csv("data/goodyears.csv", stringsAsFactors = FALSE)
+gy <- rbind(200804, 200812, gy)
+gy <- gy[order(gy$x),]
+gy <- data.frame(x = gy)
+gy$n <- unlist(lapply(gy[,1], function(x) nrow(DataflowR::grabget(x))))
+
+dt <- dplyr::left_join(dt, gy, by = c("yearmon" = "x"))
+
 dt$yearmon <- 
   sapply(dt$yearmon, function(x) paste0(substring(x, 0, 4), 
                                         "-",
                                  substring(x, 5, 6)))
 
-print(xtable(dt, caption = "Model coefficients for regressions between Dataflow and chlorophyll concentration of discrete grab samples. Also given is the coefficient of determination (R2) and p-value of each regression."
-), 
-      file = "manuscripts/est_coast/table_2.tex",
-      caption.placement = "top", 
-      include.rownames = FALSE)
+
+kable(dt, format = "latex", booktabs = TRUE, 
+      caption = "Model coefficients for regressions between Dataflow and chlorophyll concentration of discrete grab samples. Also given is the coefficient of determination (R2) and p-value of each regression.") %>% 
+  add_header_above(c(" ", "Primary" = 2, "Secondary" = 4))
+                                                                                                                                                                                                                                                                               
+
+names(dt) <- c("Date", "CDOM")
+
+# print(xtable(dt, caption = "Model coefficients for regressions between Dataflow and chlorophyll concentration of discrete grab samples. Also given is the coefficient of determination (R2) and p-value of each regression."
+# ), 
+#       file = "manuscripts/est_coast/table_2.tex",
+#       caption.placement = "top", 
+#       include.rownames = FALSE)
